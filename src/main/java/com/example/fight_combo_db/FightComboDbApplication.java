bd58a8d5a3d46121bd8cdf695b818145f70108d3
unknown <joelito140@gmail.com>
2025-07-23 20:26:21 +0200

    First Commit

---
+++ src/main/java/com/example/fight_combo_db/FightComboDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.fight_combo_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightComboDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FightComboDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fight_combo_db/FightComboDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fight_combo_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightComboDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
