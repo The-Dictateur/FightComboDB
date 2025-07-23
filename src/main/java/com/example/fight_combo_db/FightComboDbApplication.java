ccce8c038f625c432cba0b139e95d2e1c557c946
unknown <joelito140@gmail.com>
2025-07-23 23:45:30 +0200

    Main Windows .fxml

--- src/main/java/com/example/fight_combo_db/FightComboDbApplication.java
+++ src/main/java/com/example/fight_combo_db/FightComboDbApplication.java
@@ -1,13 +1,47 @@
 package com.example.fight_combo_db;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 @SpringBootApplication
-public class FightComboDbApplication {
+public class FightComboDbApplication extends javafx.application.Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FightComboDbApplication.class, args);
+	private static ConfigurableApplicationContext springContext;
+
+	@Override
+	public void init() throws Exception {
+		springContext = new SpringApplicationBuilder(FightComboDbApplication.class).run();
 	}
 
+	@Override
+	public void start(Stage stage) throws Exception {
+
+		FXMLLoader fxmlLoader = new FXMLLoader(FightComboDbApplication.class.getResource("/Main_window.fxml"));
+		fxmlLoader.setControllerFactory(springContext::getBean);
+
+		Parent root = fxmlLoader.load();
+
+		stage.setTitle("Ventana JavaFX con Spring Boot");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+	}
+
+	@Override
+	public void stop() throws Exception {
+		springContext.close();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
 }
