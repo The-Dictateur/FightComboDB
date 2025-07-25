2b0c4164d07e928aa2698f68dbf953914b9a35ec
unknown <joelito140@gmail.com>
2025-07-25 21:24:40 +0200

    fightcombo.db updated

---
+++ src/main/java/com/example/model/Juego.java
@@ -0,0 +1,33 @@
+package com.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Juego {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;  
+    }
+}
